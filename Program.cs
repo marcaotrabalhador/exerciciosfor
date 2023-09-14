1ed2b8dac416370c3a77afb76f372628c6a2e0a5
marcaotrabalhador <145072746+marcaotrabalhador@users.noreply.github.com>
2023-09-14 20:20:28 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        int countMaioresIdade = 0;
+
+        for (int i = 1; i <= 20; i++)
+        {
+            Console.Write("Digite a idade da pessoa {0}: ", i);
+            int idade = Convert.ToInt32(Console.ReadLine());
+
+            if (idade >= 18)
+            {
+                countMaioresIdade++;
+            }
+        }
+
+        Console.WriteLine("O número de pessoas maiores de idade é: " + countMaioresIdade);
+    }
+}
